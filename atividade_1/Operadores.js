005427c5a88b9936098513ba07b8287fe38a678a
nicolediaas <ninick120@gmail.com>
2025-02-28 09:33:09 -0300

    first commit

---
+++ atividade_1/Operadores.js
@@ -0,0 +1,16 @@
+
+let num_A = 30;
+let num_B = 20;
+
+let soma = num_A + num_B;
+let subtracao = num_A - num_B;
+let multiplicacao = num_A * num_B;
+let divisao = num_A / num_B;
+
+console.log(`Resultado da soma entre ${num_A} e ${num_B} = ${soma}`);
+console.log(`Resultado da subtracao entre ${num_A} e ${num_B} = ${subtracao}`);
+console.log(`Resultado da multiplicacao entre ${num_A} e ${num_B} = ${multiplicacao}`);
+console.log(`Resultado da divisao entre ${num_A} e ${num_B} = ${divisao}`);
+
+git config --global user.email "ninick120@gmail.com"
+git config --global user.name "nicolediaas"
---
+++ variaveis_tipos/variaveis_tipos.js
@@ -0,0 +1,31 @@
+console.log("Hello!!!!!");
+
+const n = 3;
+
+var j = 6;
+var j = 7;
+
+let i = 6;
+i = 7;
+
+let nome = "Nicole";
+
+let telefone = "1234";
+
+
+console.log (`Meu nome é ${nome} Telefone: ${telefone}`);
+
+let x = 1;
+x = 0.5;
+x = 3.14;
+
+let b = true;
+b = false;
+
+let conexao;
+
+if(b){
+
+    console.log(`b = ${b}`);
+}
+
