e5eade4b01f249b25b57b79e092d2abeb995e3ab
nicolediaas <ninick120@gmail.com>
2025-06-12 09:13:09 -0300

    add agendamento_consulta

--- app_agendamento_consulta/routers/agendamentoRouter.js
+++ app_agendamento_consulta/routers/agendamentoRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const agendamentoController = require('./controllers/agendamentoController');
+const agendamentoController = require('../controllers/agendamentoController');
+const usuarioController = require('../controllers/usuarioController');
 
-router.get('/', agendamentoController.getIndexView);
-router.post('/agendar_consulta', agendamentoController.postAgendarConsulta);
+router.get('/', usuarioController.verificarAutenticacao, agendamentoController.getIndexView);
+router.get('/agendamentos', usuarioController.verificarAutenticacao, agendamentoController.getAgendamentosView);
+router.post('/agendar_consulta', usuarioController.verificarAutenticacao, agendamentoController.postAgendarConsulta);
 
 module.exports = router;
